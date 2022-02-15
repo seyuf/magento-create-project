c3b578f9c112b17adc083d44c5978782461d9d29
Mamadou <seyuf@hotmail.com>
2022-02-15 21:07:44 +0100

    Update create-project.sh

--- scripts/create-project.sh
+++ scripts/create-project.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-INPUT_MAGENTO_VERSION="2.3.1"
+INPUT_MAGENTO_VERSION="2.4.1"
+
 set -e
 
 PROJECT_PATH="$(pwd)"
@@ -8,8 +9,8 @@ PROJECT_PATH="$(pwd)"
 
 echo "currently in $PROJECT_PATH"
 
-subVersion=${INPUT_MAGENTO_VERSION:2:1}
-
+majorVersion=${INPUT_MAGENTO_VERSION:2:1}
+minorVersion=${INPUT_MAGENTO_VERSION:4:1}
 
 if [ -n "$INPUT_MAGENTO_VERSION" ]
 then
@@ -20,9 +21,16 @@ then
       composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=${INPUT_MAGENTO_VERSION}
       ;;
     4)
-      update-alternatives --set php /usr/bin/php7.3 &&
-      composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=${INPUT_MAGENTO_VERSION}
-      ;;
+      case "$minorVersion" in
+        4|5)
+           update-alternatives --set php /usr/bin/php8.1 &&
+           composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=${INPUT_MAGENTO_VERSION} ;;&
+        0|1|2|3)
+           echo "Fri|Sat|Sun";;
+           update-alternatives --set php /usr/bin/php7.4 &&
+           composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=${INPUT_MAGENTO_VERSION}
+        *) ;;
+      esac
     *) echo "This version $INPUT_MAGENTO_VERSION of magento is not recognized" && exit 1 ;;
    esac
 else
@@ -32,12 +40,17 @@ fi
 
 ls -lath
 
-mv project-community-edition magento
 
-git config user.name github-actions
-git config user.email github-actions@github.com
-git add magento/.gitignore
-git commit -m 'added gitignore'
-git add magento
-git commit -m "add magento project"
-git push
+
+
+if [ -n "$INPUT_NO_PUSH" ]
+then
+  mv project-community-edition magento
+  git config user.name github-actions
+  git config user.email github-actions@github.com
+  git add magento/.gitignore
+  git commit -m 'added gitignore'
+  git add magento
+  git commit -m "add magento project to github repo"
+  git push
+fi
